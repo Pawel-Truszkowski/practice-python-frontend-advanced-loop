6337d385849a507e52bb6c086ba772d9af107cdb
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-03-30 20:24:15 +0200

    Completed task-02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,15 @@
 const x = 5;
 let result = 0;
+
+for(let i=0; i<=x; i++){
+    result += i;
+}
+
+console.log("Result: ", result);
+
+let counter = 0;
+while (counter > x){
+    result += counter;
+}
+
+console.log("Result: ", result);
